1f793395eab5a8e5b90a16e8db18d9c2869eae2f
Jeff Forcier <jeff@bitprophet.org>
2017-04-06 15:06:12 -0700

    Implement basic file selection tests

--- tests/main.py
+++ tests/main.py
@@ -16,4 +16,35 @@ class FileCollection:
         # testing, but I cannot find any outside of fnmatch_lines (which is
         # specific to this testdir stuff, and also lacks an opposite...)
         assert "somefile.py::hello_how_are_you" in stdout
+        # This wouldn't actually even happen; we'd get an ImportError instead
+        # as pytest tries importing 'someotherfile'. But eh.
         assert "whatever.txt" not in stdout
+
+    def skips_underscored_files(self, testdir):
+        testdir.makepyfile(hastests="""
+            from _util import helper
+
+            def hello_how_are_you():
+                helper()
+        """)
+        testdir.makepyfile(_util="""
+            def helper():
+                pass
+        """)
+        # TODO: why Result.str() and not str(Result)? Seems unPythonic
+        stdout = testdir.runpytest("-v").stdout.str()
+        assert "hastests.py::hello_how_are_you" in stdout
+        assert "_util.py" not in stdout
+
+    def does_not_consume_conftest_files(self, testdir):
+        testdir.makepyfile(mytests="""
+            def hello_how_are_you():
+                pass
+        """)
+        testdir.makepyfile(conftest="""
+            def this_does_nothing_useful():
+                pass
+        """)
+        stdout = testdir.runpytest("-v").stdout.str()
+        assert "mytests.py::hello_how_are_you" in stdout
+        assert "conftest.py" not in stdout
