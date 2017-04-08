6281b3455fb67eae0c37cd62bec2fb1ca7d6977d
Jeff Forcier <jeff@bitprophet.org>
2017-04-07 18:21:25 -0700

    Cut 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-relaxed',
-    version='0.0.4',
+    version='0.1.0',
     description='Relaxed test discovery/organization for pytest',
     license='BSD',
 
