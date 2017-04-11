167a92e19480640d7bfebd5b76c4536be0bc796f
Jeff Forcier <jeff@bitprophet.org>
2017-04-11 16:41:47 -0700

    Notes to self because ugh

--- tasks.py
+++ tasks.py
@@ -16,6 +16,14 @@ def coverage(c):
     """
     Run coverage with coverage.py.
     """
+    # NOTE: this MUST use coverage itself, and not pytest-cov, because the
+    # latter is apparently unable to prevent pytest plugins from being loaded
+    # before pytest-cov itself is able to start up coverage.py! The result is
+    # that coverage _always_ skips over all module level code, i.e. constants,
+    # 'def' lines, etc. Running coverage as the "outer" layer avoids this
+    # problem, thus no need for pytest-cov.
+    # NOTE: this does NOT hold true for NON-PYTEST code, so
+    # pytest-relaxed-USING modules can happily use pytest-cov.
     c.run("coverage run --source=pytest_relaxed -m pytest")
     c.run("coverage html")
     c.run("open htmlcov/index.html")
