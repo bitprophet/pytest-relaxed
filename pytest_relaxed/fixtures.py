ccc4cd487d92925f8e7a02d9ae3729c90eab72b8
Jeff Forcier <jeff@bitprophet.org>
2017-06-26 17:39:05 -0700

    Clarify a TODO

--- pytest_relaxed/fixtures.py
+++ pytest_relaxed/fixtures.py
@@ -3,10 +3,11 @@ import os
 from pytest import fixture
 
 
-# TODO: consider making this a "no param required" fixture and have uses of it
-# simply call os.environ as normal. Pro: test code looks less magical, con:
-# removes any ability to do anything more interesting with the yielded value
-# (like proxying or whatever.) See the pytest 3.1.2 docs at:
+# TODO: consider making this a "no param/funcarg required" fixture (i.e. one
+# that gets decorated onto test classes instead of injected as magic kwargs)
+# and have uses of it simply call os.environ as normal. Pro: test code looks
+# less magical, con: removes any ability to do anything more interesting with
+# the yielded value (like proxying or whatever.) See the pytest 3.1.2 docs at:
 # /fixture.html#using-fixtures-from-classes-modules-or-projects
 @fixture
 def environ():
