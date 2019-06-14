913aedb0448bb665e126ee80b3321e5f0b04b803
Jeff Forcier <jeff@bitprophet.org>
2019-06-14 10:34:40 -0400

    Actual prep for #2, drop the pytest<3.3 pin

--- setup.py
+++ setup.py
@@ -25,13 +25,8 @@ setup(
         "pytest11": ["relaxed = pytest_relaxed.plugin"]
     },
     install_requires=[
-        # NOTE: pytest 3.3 broke something, not sure what yet
-        "pytest>=3,<3.3",
-        # TODO: ditto; six is so widely used it's prob worth having a broad pin
+        "pytest>=3,<4",
         "six>=1,<2",
-        # TODO: ditto again!
-        # TODO: but also, we could make this optional if it somehow ever caused
-        # issues for anyone. Only necessary for @raises right now.
         "decorator>=4,<5",
     ],
     classifiers=[
